3a86cdebb657ef7fec49775816ceae8db461f29c
nicholas-mccrady <nicholas.mccrady@gmail.com>
2023-01-18 19:29:00 -0600

    first commit

---
+++ simple_python_scott_college_dayone.py
@@ -0,0 +1,17 @@
+x = 8
+print (x)
+stmt ='you are a swell dude'
+y = "12"
+
+#casting to int and casting to string notice that when are casting to string we are not doing a mathmatical operation so it 
+# puts the two variables together instead of adding them or performing and operation on them.##
+#print(str(x) + y)
+# note make sure that you have your formatting in the proper order
+print (str(x) + '+' + str(y) + '=' + str(int(x) + int(y)))
+#print(stmt)
+#name_user = input("what is your name\n")
+#print ('why is your name ' + str(x) + name_user + "?")
+
+input('press enter')
+
+
