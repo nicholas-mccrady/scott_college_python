f0abd4989709e5a4bb1a2dc90fddc3789d01ea2f
nicholas-mccrady <nicholas.mccrady@gmail.com>
2023-01-23 18:17:53 -0600

    second day

--- simple_python_scott_college_dayone.py
+++ simple_python_scott_college_dayone.py
@@ -1,13 +1,14 @@
-x = 8
-print (x)
-stmt ='you are a swell dude'
+stmt = "you are a swell dude, yo... so lets add"
 y = "12"
 
-#casting to int and casting to string notice that when are casting to string we are not doing a mathmatical operation so it 
+first = input('what is the first number you would like to add?: ')
+second = input('what is the second number you would like to add?: ')
+print(first,second)
+#casting to int and casting to string notice that when are casting to string we are not doing a mathematical operation so it 
 # puts the two variables together instead of adding them or performing and operation on them.##
 #print(str(x) + y)
 # note make sure that you have your formatting in the proper order
-print (str(x) + '+' + str(y) + '=' + str(int(x) + int(y)))
+#print (str(x) + '+' + str(y) + '=' + str(int(x) + int(y)))
 #print(stmt)
 #name_user = input("what is your name\n")
 #print ('why is your name ' + str(x) + name_user + "?")
